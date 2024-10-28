2f616730bc291ce0387dd0eb32cb88ffae9e1599
Amira <amira.nemdil@hotmail.com>
2024-10-28 10:37:03 +0100

    code fix

--- TP4/color.py
+++ TP4/color.py
@@ -1,24 +1,24 @@
 from PySide6.QtCore import Qt
 from PySide6.QtGui import QColor, QPalette
-from PySide6.QtWidgets import QApplication, QWidget, QMainWindow
+from PySide6.QtWidgets import QApplication, QWidget, QMainWindow, QHBoxLayout
 from layout_colors import color
 import sys
-
-class color(QWidget):
-    def __init__(self, color):
-        super().__init__()
-        self.setAutoFillBackground(True)
-        palette = self.palette()
-        palette.setColor(QPalette.Window, QColor(color))
-        self.setPalette(palette)
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("My App")
 
-        widget = color("red")
+        layout = QHBoxLayout()
+        layout.addWidget(color("red"))
+        layout.addWidget(color("black"))
+        layout.addWidget(color("yellow"))
+        
+        widget = QWidget()
+        widget.setLayout(layout)
         self.setCentralWidget(widget)
+        
+       
 
 app = QApplication(sys.argv)
 window = MainWindow()
--- TP4/layout_colors.py
+++ TP4/layout_colors.py
@@ -10,16 +10,3 @@ class color(QWidget):
         palette = self.palette()
         palette.setColor(QPalette.Window, QColor(color))
         self.setPalette(palette)
-
-class MainWindow(QMainWindow):
-    def __init__(self):
-        super().__init__()
-        self.setWindowTitle("My App")
-
-        widget = color("red")
-        self.setCentralWidget(widget)
-
-app = QApplication(sys.argv)
-window = MainWindow()
-window.show()
-app.exec()
