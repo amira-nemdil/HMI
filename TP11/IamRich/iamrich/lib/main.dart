e4a89c22e392156ecf86c9a7d2f31bdcf0d16dd6
Amira <amira.nemdil@hotmail.com>
2024-12-14 08:45:01 +0100

    i am rich app

--- TP11/IamRich/iamrich/lib/main.dart
+++ TP11/IamRich/iamrich/lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MaterialApp(home: Text("Hello niggas"),)); /MaterialApp MaterialApp is a widget that is used to create a material design app
 }
-
